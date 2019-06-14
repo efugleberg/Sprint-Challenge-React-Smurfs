0fc1b8f2bb3e22f5ee92bd5907b79c7e679ab5f5
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-06-14 10:36:28 -0500

    smurfs are rendering and post smurf added

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -14,6 +16,32 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      console.log('Get res', res);
+      this.setState({
+        smurfs: res.data
+      });
+    })
+    .catch(error => alert('error'));
+  }
+
+  postSmurf = smurf => {
+    axios
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(res => {
+      console.log('Post Smurf response', res);
+      this.setState({
+        smufs: res.data
+      });
+    })
+    .catch(err => alert('Post error'))
+  }
+
+
   render() {
     return (
       <div className="App">
